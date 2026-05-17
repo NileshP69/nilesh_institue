50c4f6b96473546718f4a1b27c41f9944bca0d30
neeraj.singh <neeraj.singh@efleetsystems.com>
2026-05-17 09:58:54 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "amar_institute";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions ? `/${repo}` : "",
+  assetPrefix: isGithubActions ? `/${repo}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
