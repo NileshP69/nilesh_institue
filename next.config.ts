b142de4de6312a1bffda62253e9a8a493188bf80
NileshP69 <npsingh2932@gmail.com>
2026-05-19 09:42:27 +0530

    Institute repo1

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let repo = "amar_institute";
+let repo = "nilesh_institue";
 
 const nextConfig: NextConfig = {
   output: "export",
